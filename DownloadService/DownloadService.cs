b28116d63c4537eeefdf213b7fd648fd2c3bb68a
Kodpencerem <emrullah04@outlook.com>
2023-06-22 09:53:29 +0300

    Add project files.

---
+++ DownloadService/DownloadService.cs
@@ -0,0 +1,16 @@
+﻿namespace DownloadService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DownloadService" in both code and config file together.
+    public class DownloadService : IDownloadService
+    {
+        public File DownloadDocument()
+        {
+            File file = new File();
+
+            file.Content = System.IO.File.ReadAllBytes(@"D:/Data/Sunu1.pptx");
+            file.Name = "Sunu1.pptx";
+
+            return file;
+        }
+    }
+}
---
+++ DownloadService/IDownloadService.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace DownloadService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDownloadService" in both code and config file together.
+
+    [ServiceContract]
+    public interface IDownloadService
+    {
+        [OperationContract]
+        File DownloadDocument();
+    }
+
+    [DataContract]
+    public class File
+    {
+        [DataMember(IsRequired = true)]
+        public string Name { get; set; }
+        [DataMember(IsRequired = true)]
+        public byte[] Content { get; set; }
+    }
+}
