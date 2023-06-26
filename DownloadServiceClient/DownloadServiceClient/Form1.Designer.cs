2c99b9192d448f6b7e3f135e3c187bc336040c23
Kodpencerem <emrullah04@outlook.com>
2023-06-26 16:58:44 +0300

    maxReceivedMessageSize, messageEncoding, WSHttpBinding, readerQuotas, maxArrayLength, servicePrincipalName

---
+++ DownloadServiceClient/DownloadServiceClient/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace DownloadServiceClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownloadFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDownloadFile
+            // 
+            this.btnDownloadFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownloadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDownloadFile.Location = new System.Drawing.Point(99, 61);
+            this.btnDownloadFile.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnDownloadFile.Name = "btnDownloadFile";
+            this.btnDownloadFile.Size = new System.Drawing.Size(715, 197);
+            this.btnDownloadFile.TabIndex = 0;
+            this.btnDownloadFile.Text = "Download File";
+            this.btnDownloadFile.UseVisualStyleBackColor = true;
+            this.btnDownloadFile.Click += new System.EventHandler(this.btnDownloadFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 414);
+            this.Controls.Add(this.btnDownloadFile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Download File System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownloadFile;
+    }
+}
+
---
+++ DownloadServiceClient/DownloadServiceClient/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DownloadServiceClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownloadFile_Click(object sender, EventArgs e)
+        {
+            DownloadService.DownladServiceClient client = new DownloadService.DownladServiceClient();
+
+            DownloadService.File file =  client.DownloadDocument();
+
+            System.IO.File.WriteAllBytes(@"D:\Data\Downloaded\" + file.Name, file.Content);
+
+            MessageBox.Show(file.Name + " is downloaded");
+
+
+        }
+    }
+}
